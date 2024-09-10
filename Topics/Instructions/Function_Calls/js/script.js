329cc4729dbcb1f500b402720771a877e95b9a12
olivax28 <olivax28@outlook.com>
2024-09-10 17:49:00 -0400

    Finished creation of blank rectangle

--- Topics/Instructions/Function_Calls/js/script.js
+++ Topics/Instructions/Function_Calls/js/script.js
@@ -1,24 +1,21 @@
 /**
- * Title of Project
- * Author Name
+ * Function Calls
+ * Olivia Axiuk
  * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ * Working on Function Calls Examples
  */
 
 "use strict";
 
-/**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
-*/
-function setup() {
 
+function setup() {
+    // One at the beginning of the program
+    createCanvas(640, 480);
 }
 
 
-/**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
-*/
 function draw() {
-
+    // Every Frame
+    background(255, 100, 100);
+    rect(200, 80, 240, 320);
 }
