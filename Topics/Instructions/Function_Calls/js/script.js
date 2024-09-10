22733ec631627f7f63630925e37ccf70e8142c29
olivax28 <olivax28@outlook.com>
2024-09-10 18:10:09 -0400

    Update script.js

--- Topics/Instructions/Function_Calls/js/script.js
+++ Topics/Instructions/Function_Calls/js/script.js
@@ -1,21 +1,29 @@
 /**
- * Function Calls
+ * The Blank Page
  * Olivia Axiuk
  * 
- * Working on Function Calls Examples
+ * An exploration of existential angst of a novelist
+ * who must sit down ar their pink desk and confront the abyss of the plank page
+ * 
+ * Program is not interactive.
  */
 
 "use strict";
 
-
+/**
+ * Creates a canvas for our piece
+ */
 function setup() {
-    // One at the beginning of the program
+    // Create the canvas at a standard resolution
     createCanvas(640, 480);
 }
 
-
+/**
+ * Draws the Writer's desktop and the blank piece of paper
+ */
 function draw() {
-    // Every Frame
+    // The pink desktop
     background(255, 100, 100);
+    // The blank piece of paper
     rect(200, 80, 240, 320);
 }
