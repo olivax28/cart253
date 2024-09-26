a4b34569434508a51184b1266311409f4c6b7e67
olivax28 <olivax28@outlook.com>
2024-09-26 14:59:22 -0400

    got the goldfish as an orange dot to follow the mouse

--- Topics/Art Jam/js/script.js
+++ Topics/Art Jam/js/script.js
@@ -1,6 +1,6 @@
 /**
  * Gluttonous Goldfish
- * Author Name
+ * Olivia Axiuk
  * 
  * A goldfish that really, really, really loves food, so much it gets bigger 
  * and bigger until it dies....
@@ -10,21 +10,49 @@
 
 // Descriptors of the goldfish
 let goldfish = {
+        color: {
+            r:255,
+            g:165,
+            b:0
+        },
+    shape: {
+        x: undefined,
+        y: undefined,
+        size: 25
+    },
     
 }
+
+// const food = {}
 
 
 /**
  * OH LOOK I DIDN'T DESCRIBE SETUP!!
 */
 function setup() {
+    createCanvas(640,400);
+    noCursor();
 
 }
 
+function moveGoldfish() {
+    goldfish.shape.x = mouseX;
+    goldfish.shape.y = mouseY;
+}
 
 /**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+ * draws the pale water of the aquarium
 */
 function draw() {
-
+    background(173,216,230);
+    moveGoldfish();
+    drawGoldfish();
+}
+// draws the goldfish which follows the mouse
+function drawGoldfish() {
+    push();
+    fill(goldfish.color.r, goldfish.color.g, goldfish.color.b);
+    noStroke();
+    ellipse(goldfish.shape.x, goldfish.shape.y,goldfish.shape.size);
+    pop();
 }
