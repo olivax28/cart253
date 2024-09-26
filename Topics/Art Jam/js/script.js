e3e3381874899385af4e542180c6639f251ebd4d
olivax28 <olivax28@outlook.com>
2024-09-26 13:19:15 -0400

    created Art Jam Goldfish file

---
+++ Topics/Art Jam/js/script.js
@@ -0,0 +1,30 @@
+/**
+ * Gluttonous Goldfish
+ * Author Name
+ * 
+ * A goldfish that really, really, really loves food, so much it gets bigger 
+ * and bigger until it dies....
+ */
+
+"use strict";
+
+// Descriptors of the goldfish
+let goldfish = {
+    
+}
+
+
+/**
+ * OH LOOK I DIDN'T DESCRIBE SETUP!!
+*/
+function setup() {
+
+}
+
+
+/**
+ * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+*/
+function draw() {
+
+}
