b9083b72152c19c19012fceb076ea1787a0c78c2
olivax28 <olivax28@outlook.com>
2024-09-26 16:06:46 -0400

    have a goldfish with an eye and static food

--- Topics/Art Jam/js/script.js
+++ Topics/Art Jam/js/script.js
@@ -18,16 +18,22 @@ let goldfish = {
     shape: {
         x: undefined,
         y: undefined,
-        size: 25
+        size: 30
     },
     
 }
-
-// const food = {}
-
+// descriptors of the fish food
+let fishFood = {
+    color: "#A52A2A",
+    shape: {
+    x: 320,
+    y: 200,
+    size: 30
+    },
+}
 
 /**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
+ * 
 */
 function setup() {
     createCanvas(640,400);
@@ -35,19 +41,23 @@ function setup() {
 
 }
 
-function moveGoldfish() {
-    goldfish.shape.x = mouseX;
-    goldfish.shape.y = mouseY;
-}
+
 
 /**
- * draws the pale water of the aquarium
+ * draws the pale water of the aquarium and the fish
 */
 function draw() {
     background(173,216,230);
     moveGoldfish();
     drawGoldfish();
+    drawfishFood();
 }
+
+function moveGoldfish() {
+    goldfish.shape.x = mouseX;
+    goldfish.shape.y = mouseY;
+}
+
 // draws the goldfish which follows the mouse
 function drawGoldfish() {
     push();
@@ -55,4 +65,16 @@ function drawGoldfish() {
     noStroke();
     ellipse(goldfish.shape.x, goldfish.shape.y,goldfish.shape.size);
     pop();
+    ellipse(goldfish.shape.x + goldfish.shape.size/3, goldfish.shape.y, goldfish.shape.size/10)
 }
+
+// draws the fish food
+function drawfishFood() {
+    push();
+    fill(fishFood.color);
+    noStroke();
+    square(fishFood.shape.x, fishFood.shape.y, fishFood.shape.size);
+    pop();
+    console.log(fishFood.shape.x, fishFood.shape.y, fishFood.shape.size);
+}
+
