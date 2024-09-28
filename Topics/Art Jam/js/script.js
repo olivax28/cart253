72f7c8381d1bba4ff5971db5d3da701c68006bff
olivax28 <olivax28@outlook.com>
2024-09-28 14:47:58 -0400

    constrained the Green value as the Goldfish eats

--- Topics/Art Jam/js/script.js
+++ Topics/Art Jam/js/script.js
@@ -3,32 +3,34 @@
  * Olivia Axiuk
  * 
  * A goldfish that really, really, really loves food, so much it gets bigger 
- * and bigger until it dies....
+ * and redder until it dies....
  */
 
 "use strict";
 
 // Descriptors of the goldfish
 let goldfish = {
-        color: {
-            r:255,
-            g:165,
-            b:0
-        },
+    color: {
+        r: 255,
+        g: 165,
+        b: 0,
+        minG: 0,
+        maxG: 255
+    },
     shape: {
         x: undefined,
         y: undefined,
         size: 30
     },
-    
+
 }
 // descriptors of the fish food
 let fishFood = {
     color: "#A52A2A",
     shape: {
-    x: 320,
-    y: 200,
-    size: 30
+        x: 320,
+        y: 200,
+        size: 30
     },
 }
 
@@ -36,7 +38,7 @@ let fishFood = {
  * 
 */
 function setup() {
-    createCanvas(640,400);
+    createCanvas(640, 400);
     noCursor();
 
 }
@@ -47,11 +49,11 @@ function setup() {
  * draws the pale water of the aquarium and the fish
 */
 function draw() {
-    background(173,216,230);
+    background(173, 216, 230);
     moveGoldfish();
     checkEatingFood();
+    drawFishFood();
     drawGoldfish();
-    drawfishFood();
 }
 
 function moveGoldfish() {
@@ -59,16 +61,24 @@ function moveGoldfish() {
     goldfish.shape.y = mouseY;
 }
 
-function checkEatingFood(){
+function checkEatingFood() {
     //distance between mouse which fish follows and food
-    const distance = dist(goldfish.shape.x,goldfish.shape.y,fishFood.shape.x,fishFood.shape.y);
+    const distance = dist(goldfish.shape.x, goldfish.shape.y, fishFood.shape.x, fishFood.shape.y);
     // if mouse overlaps the food
-    const mouseIsOverlapping = (distance < fishFood.shape.size/2);
+    const mouseIsOverlapping = (distance < fishFood.shape.size / 2);
     // check if the mouse which controls the fish has touched the food
     if (mouseIsOverlapping) {
         goldfish.color.g -= 1
     }
-    // NOTE TO SELF CONSTRAIN GREEN
+}
+
+// draws the fish food
+function drawFishFood() {
+    push();
+    fill(fishFood.color);
+    noStroke();
+    ellipse(fishFood.shape.x, fishFood.shape.y, fishFood.shape.size);
+    pop();
 }
 
 // draws the goldfish which follows the mouse
@@ -77,24 +87,16 @@ function drawGoldfish() {
     push();
     fill(goldfish.color.r, goldfish.color.g, goldfish.color.b);
     noStroke();
-    ellipse(goldfish.shape.x, goldfish.shape.y,goldfish.shape.size);
+    ellipse(goldfish.shape.x, goldfish.shape.y, goldfish.shape.size);
     pop();
     // eye
     push();
-    ellipse(goldfish.shape.x + goldfish.shape.size/3, goldfish.shape.y, goldfish.shape.size/10)
+    ellipse(goldfish.shape.x + goldfish.shape.size / 3, goldfish.shape.y, goldfish.shape.size / 10)
     pop();
     // tail
     push();
-    triangle(goldfish.shape.x + goldfish.shape.size/2, goldfish.shape.y+ goldfish.shape.size/2,goldfish.shape.x + goldfish.shape.size/-0.7, goldfish.shape.y+ goldfish.shape.size/-0.7,goldfish.shape.x + goldfish.shape.size/-2, goldfish.shape.y+ goldfish.shape.size/-2);
+    fill(goldfish.color.r, goldfish.color.g, goldfish.color.b);
+    triangle(goldfish.shape.x + goldfish.shape.size / 2, goldfish.shape.y + goldfish.shape.size / 2, goldfish.shape.x + goldfish.shape.size / 2, goldfish.shape.y + goldfish.shape.size / -0.7, goldfish.shape.x + goldfish.shape.size / -2, goldfish.shape.y + goldfish.shape.size / -2);
     pop();
+    goldfish.color.g = constrain(goldfish.color.g, goldfish.color.minG, goldfish.color.maxG)
 }
-
-// draws the fish food
-function drawfishFood() {
-    push();
-    fill(fishFood.color);
-    noStroke();
-    ellipse(fishFood.shape.x, fishFood.shape.y, fishFood.shape.size);
-    pop();
-} 
-
