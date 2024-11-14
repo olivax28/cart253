ab86f6f656e760d1b64cfbca9826d7f4191d521f
olivax28 <olivax28@outlook.com>
2024-11-13 21:28:35 -0500

    set up some basic variables to begin work

--- Topics/Variations_jam/js/script.js
+++ Topics/Variations_jam/js/script.js
@@ -7,6 +7,50 @@
 
 "use strict";
 
+let playerShip = {
+    body: {
+        x: 320,
+        y: 420,
+        sprite: undefined,
+
+    },
+    // The gun's spray has a position, size, speed, and state
+    bullet: {
+        x: undefined,
+        y: 480,
+        size: 10,
+        speed: 30,
+        // Determines how the spray moves each frame
+        state: "idle" // State can be: idle, outbound, inbound
+    },
+
+
+
+}
+
+
+
+
+// The targets, hit to gain points
+// Has a position, size, and speed of horizontal movement as well as two levels of fill (red)
+const spaceInvader = {
+    x: 100, // will be random
+    y: 0,
+    size: 50,
+    speed: 2
+};
+
+// the red healthbar
+let healthBar = {
+    x: 20,
+    y: 5,
+    HealthlvlWidth: 100,
+    maxWidth: 100,
+    h: 30,
+    color: "#7eff1b"
+}
+
+
 /**
  * OH LOOK I DIDN'T DESCRIBE SETUP!!
 */
