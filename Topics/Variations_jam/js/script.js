ff4d088c2c46589658384fe59beaa96f7006d2db
olivax28 <olivax28@outlook.com>
2024-11-13 21:14:37 -0500

    created variation jam "Astral Projectile" project

---
+++ Topics/Variations_jam/js/script.js
@@ -0,0 +1,26 @@
+/**
+ * Astral Projectiles
+ * Olivia Axiuk
+ * 
+ *(GAME DESCRIPTION HERE)
+ */
+
+"use strict";
+
+/**
+ * OH LOOK I DIDN'T DESCRIBE SETUP!!
+*/
+function setup() {
+    createCanvas(1080, 720);
+
+
+}
+
+
+/**
+ * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+*/
+function draw() {
+    background(0, 0, 0);
+
+}
